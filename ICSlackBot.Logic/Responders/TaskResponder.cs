2cb6a114c21f36e3596fa6fb21c6ba400669bf05
Danysan <dany@danysan.com>
2019-03-16 12:21:07 +0100

    Clean up responder

--- ICSlackBot.Logic/Responders/TaskResponder.cs
+++ ICSlackBot.Logic/Responders/TaskResponder.cs
@@ -1,6 +1,13 @@
 ﻿using SlackBot.Models;
 using SlackBot.Responders;
 using ICSlackBot.Logic.Controllers;
+using ICSlackBot.Logic.Connectors;
+using ICSlackBot.Logic.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Newtonsoft.Json.Linq;
+using System.Net;
 
 namespace ICSlackBot.Logic.Responders
 {
@@ -9,13 +16,14 @@ namespace ICSlackBot.Logic.Responders
     /// </summary>
     public class TaskResponder : IResponder
     {
-
-        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private SlackBotController botController;
+        private static readonly log4net.ILog _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private SlackBotController _botController;
+        private IcZeroBotConnector _icZeroBotConnector;
 
         public TaskResponder(SlackBotController botController)
         {
-            this.botController = botController;
+            _botController = botController;
+            _icZeroBotConnector = new IcZeroBotConnector(botController.cfg.Get("ZeroBot", "BusinessLogicEndPoint", "http://ic-zerobot.infocentric.ch/api"));
         }
 
         public TaskResponder() : this(null) { }
@@ -27,7 +35,7 @@ namespace ICSlackBot.Logic.Responders
         /// <returns></returns>
         public bool CanRespond(ResponseContext context)
         {
-            return true;
+            return context.Message.ChatHub.Type == SlackChatHubType.DM;
         }
 
         /// <summary>
@@ -37,11 +45,63 @@ namespace ICSlackBot.Logic.Responders
         /// <returns></returns>
         public BotMessage GetResponse(ResponseContext context)
         {
-            BotMessage botMessage = new BotMessage() { Text = "Yeeeyyy, we answer" };
+            BotMessage botMessage = new BotMessage() { Text = "Didn't work :(" };
+            try
+            {
+                //TODO: add switch logic depending on what the bot should to
+                if (true)
+                {
+                    botMessage.Text = CreateTask(context.Message);
+                }
+            }
+            catch (Exception ee)
+            {
+                botMessage.Text += $" {ee.Message}";
+            }
             return botMessage;
         }
 
-    
+        /// <summary>
+        /// Creates the task.
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
+        /// <returns></returns>
+        private string CreateTask(SlackMessage msg)
+        {
+            IcZeroBotTask task = new IcZeroBotTask()
+            {
+                Title = msg.Text.Replace("create task ", ""),
+                Description = msg.Text.Replace("create task ", ""),
+                Creator = msg.User?.Name ?? "Unidentified user. Probably new."
+            };
+
+            List<byte[]> atta = new List<byte[]>();
+
+            dynamic d = JObject.Parse(msg.RawData);
+
+            if (d.files != null && d.files.Count > 0)
+            {
+                var webClient = new WebClient();
+                webClient.Headers.Add("Authorization", $"Bearer #{this._botController.cfg.Get("Slack", "ApiToken", "xxx")}");
+
+                foreach (dynamic f in d.files)
+                {
+                    byte[] img = webClient.DownloadData(f.url_private.ToString());
+                    atta.Add(img);
+                }
+            }
+
+            if (atta.Any())
+            {
+                task.Attachements = atta;
+            }
+
+            string uid = _icZeroBotConnector.CreateTask(task);
+
+            return $"Yeeeyyy, task created ${uid}";
+        }
+
+
         /// <summary>
         /// Gets the command description.
         /// </summary>
